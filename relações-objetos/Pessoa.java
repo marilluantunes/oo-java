2ffcfa631b349970e10b6e87a8d9ae28220d9904
Maria Luiza <marialluantunes@gmail.com>
2025-09-29 00:49:39 -0300

    class Pessoa

---
+++ relações-objetos/Pessoa.java
@@ -0,0 +1,9 @@
+class Pessoa {
+    String nome, cpf;
+    Veiculo automovel;
+
+    void comprarVeiculo(Veiculo v){
+        automovel = v;
+        System.out.println(nome+ "comprou o veículo:" + v.modelo);
+    }
+}
