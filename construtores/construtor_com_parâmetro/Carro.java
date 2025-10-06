fa31f7021785fe9eca3919fa0675546b0a857826
Maria Luiza <marialluantunes@gmail.com>
2025-10-06 17:27:03 -0300

    construtores

---
+++ construtores/construtor_com_parâmetro/Carro.java
@@ -0,0 +1,15 @@
+package construtor_com_parâmetro;
+
+public class Carro {
+
+    String marca;
+    String cor;
+
+    //construtor alternativo
+
+    public Carro(String m , String c){
+        marca = m;
+        cor = c;
+    }
+    
+}
---
+++ construtores/construtor_com_parâmetro/Main.java
@@ -0,0 +1,11 @@
+package construtor_com_parâmetro;
+
+public class Main {
+    public static void main(String[] args) {
+        Carro carro1 = new Carro("Ford" , "Azul");
+        System.out.println(carro1.marca);
+        System.out.println(carro1.cor);
+
+    }
+    
+}
