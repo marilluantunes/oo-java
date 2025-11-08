4667fd7adadf4fef2566bfe02009fb9c20f0bc77
Maria Luiza <marialluantunes@gmail.com>
2025-11-08 13:17:16 -0300

    class mãe  Veiculo

---
+++ herança/Veiculo.java
@@ -0,0 +1,22 @@
+package herança;
+
+// CLASSE MAE 
+
+public class Veiculo {
+    String marca;
+    String modelo;
+    int anoFab;
+    int anoMod;
+    int numOcupantes;
+
+    void ligar() {
+        System.out.println("o veiculo easta ligado!");
+    }
+
+    void desligar(){
+        System.out.println("o veiculo esta desligado!");
+    }
+
+    
+}
+
