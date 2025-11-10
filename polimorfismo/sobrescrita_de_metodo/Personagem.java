80de4ea09f36377915f6cc2f1f0628277f1b63b4
Maria Luiza <marialluantunes@gmail.com>
2025-11-10 16:54:44 -0300

    class mae  Personagem

---
+++ polimorfismo/sobrescrita_de_metodo/Personagem.java
@@ -0,0 +1,11 @@
+package polimorfismo.sobrescrita_de_metodo;
+
+// CLASSE MAE
+public class Personagem {
+
+    public void atacar(){
+        System.out.println("o personagem ataca!");
+    }
+    
+}
+
